a218a4af831740f8d8997caf014aaf206091597f
chandru793 <67958864+chandru793@users.noreply.github.com>
2023-09-07 20:26:07 +0530

    Basic coding

---
+++ Basic Coding Problems/Reverse a Number/revnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class revnum {
+    public static void main(String[] args) {
+        int num = 213516;
+        int rev = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println(rev);
+    }
+}
